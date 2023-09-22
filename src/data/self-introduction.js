7e13021c7609c431bc9206dc8da1665ada95c62f
yingtu35 <daniel860305@gmail.com>
2023-09-22 15:24:27 -0700

    update resume

--- src/data/self-introduction.js
+++ src/data/self-introduction.js
@@ -1,4 +1,4 @@
 export const selfIntro = `Transition into Computer Science in late 2021 and the journey continues.
 Looking for Summer 2024 internship opportunities`
 
-export const resume = "https://drive.google.com/file/d/1KWRD9asW4epCL_FIjP__2COv5SAC01dZ/view?usp=sharing"
+export const resume = "https://drive.google.com/file/d/118ibKSlfyXHcZq6yNLLpWhcQusUY__Gz/view?usp=sharing"
